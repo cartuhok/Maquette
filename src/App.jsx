bd804105efe8670e1abf9460f393e2ca5301977a
Carter Rink <carter.m.rink@gmail.com>
2023-08-22 13:42:56 -0400

    Added Theatre.js camera scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
 import { Canvas, useFrame } from "@react-three/fiber"
-import { Gltf, ScrollControls, useScroll } from "@react-three/drei"
+import { Gltf, ScrollControls, useScroll, Scroll } from "@react-three/drei"
 import { getProject, val } from "@theatre/core"
+import flyThroughState from "./state.json"
 import House from './House'
+
 
 import {
     SheetProvider,
     PerspectiveCamera,
     useCurrentSheet,
-  } from "@theatre/r3f";
+  } from "@theatre/r3f"
 
 //#D28D4B
 
 export default function App() {
-    const sheet = getProject("Fly Through").sheet("Scene")
+  const sheet = getProject("Fly Through", {state: flyThroughState}).sheet("Scene")
 
     return (
     <Canvas gl={{ preserveDrawingBuffer: true }}>
-      <ScrollControls pages={5}>
+      <ScrollControls pages={5} >
         <SheetProvider sheet={sheet}>
           <House />
         </SheetProvider>
       </ScrollControls>
     </Canvas>
-  );
+  )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import studio from "@theatre/studio"
-import extension from "@theatre/r3f/dist/extension"
+// import studio from "@theatre/studio"
+// import extension from "@theatre/r3f/dist/extension"
 import React, { Suspense } from "react"
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './App.css'
 
-studio.extend(extension)
-studio.initialize()
+// studio.extend(extension)
+// studio.initialize()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
