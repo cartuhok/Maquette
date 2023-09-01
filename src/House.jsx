59919ac6df06bafb56d209f192a0343039704634
Carter Rink <carter.m.rink@gmail.com>
2023-09-01 12:03:43 -0400

    Changed file paths for gh-pages

--- src/House.jsx
+++ src/House.jsx
@@ -21,7 +21,7 @@ import {
 export default function House(props) {
   const sheet = useCurrentSheet()
   const scroll = useScroll()
-  const { nodes, materials } = useGLTF("/modelHouse.glb")
+  const { nodes, materials } = useGLTF("./models/modelHouse.glb")
   const roofRef = useRef()
   const snowholderRef = useRef()
   const snowholderMountRef = useRef()
@@ -284,4 +284,4 @@ export default function House(props) {
   )
 }
 
-useGLTF.preload("/modelHouse.glb")
+useGLTF.preload("./models/modelHouse.glb")
